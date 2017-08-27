8c78f6b443d7d166320437d139a363fed2cce00c
atul2 <atul19971@gmail.com>
2017-08-27 19:51:17 +0530

    SQL commit

---
+++ sql/crypto4ora.sql
@@ -0,0 +1,19 @@
+
+EXEC dbms_java.grant_permission( 'ATUL', 'SYS:java.util.PropertyPermission', 'java.class.path', 'write' );
+EXEC dbms_java.grant_permission( 'ATUL', 'SYS:java.io.FilePermission', '*', 'read' );
+
+CREATE OR REPLACE PACKAGE CRYPTO AS 
+FUNCTION RSA_ENCRYPT(PLAIN_TEXT VARCHAR2,PUBLIC_KEY VARCHAR2) RETURN VARCHAR2
+AS
+LANGUAGE JAVA NAME 'com/dishtavar/crypto4ora/RSAUtil.encrypt (java.lang.String,java.lang.String) return java.lang.String';
+FUNCTION RSA_DECRYPT(ENCRYPTED_TEXT VARCHAR2,PRIVATE_KEY VARCHAR2) RETURN VARCHAR2
+AS
+LANGUAGE JAVA NAME 'com/dishtavar/crypto4ora/RSAUtil.decrypt (java.lang.String,java.lang.String) return java.lang.String';
+FUNCTION RSA_GENERATE_KEYS(KEY_SIZE NUMBER) RETURN VARCHAR2
+AS
+LANGUAGE JAVA NAME 'com/dishtavar/crypto4ora/GenerateKey.generateRSAKeys (java.lang.Integer) return java.lang.String';
+
+END CRYPTO;
+/
+
+--loadjava -f -verbose -oracleresolver -resolve -oci8 -u user/password@service crypto4ora.jar
